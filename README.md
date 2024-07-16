57456dfa4f1a97b77c932db22ea14f8c78a8f954
Kelsey Frahm <k.rae.heider@gmail.com>
2024-07-16 18:26:25 -0500

    added helper files and wrote post code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# note-taker
+# Note Taker
+
+## Description
+
+This weather website allows you to search cities and get the current weather as well as the 5 day forecast for that city. it also shows your previously searched cities. 
+
+## Usage
+Open server file in your terminal and run the server with 'node server.js' once the server is running open the link in your browser. This will run the html files. 
+
+For repository: https://github.com/krfrahm/note-taker.git
+For website: https://krfrahm.github.io/note-taker/
+
+
+![Website screenshot 1](assets\images\weather-dash.PNG)
+
+
+## Credits
+
+This project is soley my work and the provided materials. I also looked at several stack overflow message boards to problem solve with my code. 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
+const uuid = require('./helpers/uuid.js');
 
 
 const app = express();
@@ -22,6 +23,37 @@ app.get('/api/notes', (req,res) => {
     return res.json(notes)
 })
 
+app.post('/api/notes', (req,res) => {
+    const {noteTitle, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            noteTitle,
+            text,
+            note_id: uuid()
+        }
+    const noteString = JSON.stringify(newNote);
+
+    // Write the string to a file
+    fs.writeFile(`./db/${newNote.product}.json`, noteString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newNote.product} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+})
+
 app.listen(PORT, () =>
-    console.log('app listening at http://localhost:${PORT}')
+    console.log(`app listening at http://localhost:${PORT}`)
 )
